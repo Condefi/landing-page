83c21274b9cb248c6b1a83aca985fbeb300557dc
Owklama <owklamaa@gmail.com>
2024-11-11 18:58:05 +0700

    chore: changed images from png to svg

--- components/DownloadApp/index.tsx
+++ components/DownloadApp/index.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import appStore from "@/assets/app-store.png";
-import playStore from "@/assets/google-play.png";
+import appStore from "@/assets/app-store.svg";
+import playStore from "@/assets/google-play.svg";
 import Image from "next/image";
 
 const DownloadApp = () => {
   return (
     <div className="py-10 md:py-20 px-4 md:px-24 bg-background">
       <div className="max-w-7xl mx-auto">
-        <div className="text-center mb-6 md:mb-8">
+        <div className="text-center">
           <h2 className="text-2xl md:text-4xl bg-clip-text text-transparent bg-gradient-to-r from-[hsl(var(--gradient-start))] to-[hsl(var(--gradient-end))] text-center font-sans font-bold">
             Download Our Mobile App
           </h2>
@@ -17,16 +17,16 @@ const DownloadApp = () => {
           </p>
         </div>
 
-        <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+        <div className="flex flex-col md:flex-row justify-center items-start gap-4">
           <Image
             src={appStore}
             alt="App Store"
-            className="w-40 md:w-32 cursor-pointer"
+            className="w-40 h-auto object-contain cursor-pointer"
           />
           <Image
             src={playStore}
             alt="Play Store"
-            className="w-40 md:w-32 cursor-pointer"
+            className="w-40 cursor-pointer"
           />
         </div>
       </div>
